dff15aa7093eebdf6d303d59c80f3e9fb9234f4b
Joshua Jolley <jolleyboy@gmail.com>
2024-01-23 23:06:04 -0700

    Add configurable port

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,12 @@ use rouille::Response;
 fn main() {
     let in_file = env::var("ENSHROUDED_PLAYER_COUNT_FILE")
         .unwrap_or_else(|_e| "./enshrouded-player-count.txt".to_string());
-    rouille::start_server("0.0.0.0:8080", move |request| {
+
+    let port = env::var("ENSHROUDED_PLAYER_COUNT_SERVER_PORT")
+        .unwrap_or_else(|_e| "8080".to_string());
+
+
+    rouille::start_server(format!("{}{}", "0.0.0.0:", port), move |_request| {
         let player_count = fs::read_to_string(&in_file).expect("Unable to read file");
         Response::text(player_count)
     })
